62751c102d7a5e29ca770c5f85ab948a51dc59bf
jcoenencom <jpcoenen@gmail.com>
2025-01-19 10:26:39 +0100

    Update motiongw.py
    Modified the scan routine
    if IP not defined then do a discovery
    if IP and KEY defined then do a getDeviciceList. from the GW

--- motiongw.py
+++ motiongw.py
@@ -5,12 +5,6 @@ from motionblinds import MotionDiscovery
 from .. import fhem, generic
 
 
-DEVICE_TYPES_GATEWAY = ["02000001", "02000002"]  # Gateway
-DEVICE_TYPE_BLIND = "10000000"  # Standard Blind
-DEVICE_TYPE_TDBU = "10000001"  # Top Down Bottom Up
-DEVICE_TYPE_DR = "10000002"  # Double Roller
-
-
 class motiongw(generic.FhemModule):
 
     devtype = {"02000001":"Gateway", "02000002":"Gateway","10000000":"Standard Blind", "10000001":"Top/Down Bottom/Up", "10000002":"Double Roller"}
@@ -116,47 +110,75 @@ class motiongw(generic.FhemModule):
             d = MotionDiscovery()
             mesg = d.discover()
             return d
+        
+    async def __getDevList(self):
+        # create the MotionGateway instance
+        # sim mode, populated devices in gw instance
+        # live request the devlist to the gateway
+        self.gw = MotionGateway(ip=self.IP, key=self.key)
+        self.logger.debug(f"Internal getDevList: instanciate gw IP=(self.IP) KEY=(self.key)")
 
+        if self.mode == "sim":
+            obj1 = MotionBlind(gateway=self.gw, mac='123456789', device_type='RollerBlind')
+            obj2 = MotionBlind(gateway=self.gw, mac='987654321', device_type='RollerBlind')
+            # assign the object to the gateway device list, the key is the Mac address
+            self.gw.device_list[obj1.mac]=obj1
+            self.gw.device_list[obj2.mac]=obj2
+            self.logger.debug(f"Internal getDevList: sim defined 2 blinds in gw")
+            return
+        else:
+            self.logger.debug(f"Internal getDevList: live interogate gw")
+            self.gw.GetDeviceList()
+            self.gw.Update()
+            return
+        
     async def set_scan(self, hash, params):
-        # Either scan for a gateway (no IP defined)
-        # Or scan for devices
-        if self.key == None:
-            return "Define key attribute first"
+        # Perform a discovery calling internal __discover routine that discriminate between sim and live mode
+        # IP beig the first parameter of the define in fhem, if not set, key is also not set
         if self.IP == None:
-            self.logger.info(f"calling discover routine")
+            # no IP run a discovery
+            self.logger.debug(f"calling discover routine")
             mesg = await self.__discover()
             # normally mesg holds the complete configuration of the discovered gateway/hub
-            self.logger.info(mesg)
-        else:
-            self.logger.info(f"Getting Device info from the gateway")
-            # IP is defined run a GetDevice
-            # the discovery returns a disct for ecery gateway found
-            # the ip address os the gateway is thus: list(mesg.keys())[0]
-        self.IP = list(mesg.keys())[0]
-        hash['IP']=self.IP
-        await fhem.readingsBeginUpdate(hash)
-        for key in mesg.keys():
-            await fhem.readingsBulkUpdateIfChanged(self.hash, key, "GW IP defined")
-            await fhem.readingsBulkUpdateIfChanged(self.hash, "state", "Discovery done")
-            for k in mesg['192.168.1.100']['data']:
-                await fhem.readingsBulkUpdateIfChanged(self.hash, k['mac'], self.devtype[k['deviceType']])
-                if (k['deviceType'] == '10000000'):
-                    # this is a standard blind, define the  fhempy device
-                    await fhem.CommandDefine(
-                        self.hash,
-                        (
-                            f"motionblinds_{k['mac']} fhempy motionblinds "
-                            f"{self.IP} {self.key} {k['mac']} "
-                            f"{k['deviceType']} "
-                        ),
-                    )
-                elif (k['deviceType'] == '02000002'):
-                # the gateway itself
-                    self.gateway = MotionGateway(ip = self.IP, key = self.key)
-                    hash['mac'] = k['mac']
-
-
-            await fhem.readingsEndUpdate(hash, 1)
+            self.logger.debug(mesg)
+            # get the IP from the discover message and set it on the gw obj
+            self.IP = list(mesg.keys())[0]
+            hash['IP']=self.IP
+            await fhem.readingsBeginUpdate(hash)
+            for key in mesg.keys():
+                await fhem.readingsBulkUpdateIfChanged(self.hash, "state", "Discovery done")
+                # define each device known to the HUB as motionblinds instance of fhem
+                for k in mesg[self.IP]['data']:
+                    await fhem.readingsBulkUpdateIfChanged(self.hash, k['mac'], self.devtype[k['deviceType']])
+                    if (k['deviceType'] == '10000000'):
+                        # this is a standard blind, define the  fhempy device
+                        await fhem.CommandDefine(
+                            self.hash,
+                            (
+                                f"motionblinds_{k['mac']} fhempy motionblinds "
+                                f"{self.IP} {self.key} {k['mac']} "
+                                f"{k['deviceType']} "
+                            ),
+                        )
+                    elif (k['deviceType'] == '02000002'):
+                    # the gateway itself
+                        self.gateway = MotionGateway(ip = self.IP, key = self.key)
+                        hash['mac'] = k['mac']
+                await fhem.readingsEndUpdate(hash, 1)
+        elif self.key != None :
+            # IP and key are set we can do a GetDeviceList
+            self.logger.info(f"Getting Device info from the gateway (self.IP)")
+            mesg = await self.__discover()
+            for blind in self.gw.device_list.values():
+                self.logger.debug(f"gw scan: device_list.value loop: device mac (blind.mac)")
+                await fhem.CommandDefine(
+                    self.hash,
+                    (
+                        f"motionblinds_{blind.mac}) fhempy motionblinds "
+                        f"{self.IP} {self.key} {blind.mac} "
+                        f"{blind.device_type} "
+                    ),
+                )
 
     async def set_key(self, hash, params):
         # attribute was set to self._key_interval
@@ -171,4 +193,3 @@ class motiongw(generic.FhemModule):
         mode = params["mode"]
         await fhem.readingsSingleUpdate(hash, "mode", mode, 1)
         self.mode = mode
-
